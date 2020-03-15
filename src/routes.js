f45bde1880c74d9dea6eec7ce1011d74a4e6a499
Marlon Menegatti <devmenegatti@gmail.com>
2020-03-14 23:34:06 -0300

    Configuração de rotas

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from "react-router-dom";
+
+import Home from './pages/Home';
+import Card from './pages/Card';
+
+export default function Routes() {
+  return(
+   <Switch>
+     <Route exact path="/" component={Home} />
+     <Route path="/card" component={Card} />
+   </Switch>
+  );
+}
