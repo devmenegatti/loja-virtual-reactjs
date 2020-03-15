8254b93fb32f8bd19a929e9941f54812e3c100d3
Marlon Menegatti <devmenegatti@gmail.com>
2020-03-15 01:40:24 -0300

    Criação e estilização da pagina Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,89 @@
 import React from 'react';
+import { MdAddShoppingCart } from "react-icons/md";
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <h1>Inicio</h1>
+    <ProductList>
+      <li>
+        <img src="https://images-na.ssl-images-amazon.com/images/I/41h9X8HpvEL._SX425_.jpg" alt="Smartphone"/>
+        <strong>Smartphone Xiaomi Redmi Note 8 64GB Versão Global Desbloqueado Preto</strong>
+        <span>R$1099,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://images-na.ssl-images-amazon.com/images/I/41h9X8HpvEL._SX425_.jpg" alt="Smartphone"/>
+        <strong>Smartphone Xiaomi Redmi Note 8 64GB Versão Global Desbloqueado Preto</strong>
+        <span>R$1099,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://images-na.ssl-images-amazon.com/images/I/41h9X8HpvEL._SX425_.jpg" alt="Smartphone"/>
+        <strong>Smartphone Xiaomi Redmi Note 8 64GB Versão Global Desbloqueado Preto</strong>
+        <span>R$1099,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://images-na.ssl-images-amazon.com/images/I/41h9X8HpvEL._SX425_.jpg" alt="Smartphone"/>
+        <strong>Smartphone Xiaomi Redmi Note 8 64GB Versão Global Desbloqueado Preto</strong>
+        <span>R$1099,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://images-na.ssl-images-amazon.com/images/I/41h9X8HpvEL._SX425_.jpg" alt="Smartphone"/>
+        <strong>Smartphone Xiaomi Redmi Note 8 64GB Versão Global Desbloqueado Preto</strong>
+        <span>R$1099,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://images-na.ssl-images-amazon.com/images/I/41h9X8HpvEL._SX425_.jpg" alt="Smartphone"/>
+        <strong>Smartphone Xiaomi Redmi Note 8 64GB Versão Global Desbloqueado Preto</strong>
+        <span>R$1099,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
   );
 }
