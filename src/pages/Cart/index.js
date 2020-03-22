862b7ac34e8829ff604177a44ea9f87e0eec76d3
Marlon Menegatti <devmenegatti@gmail.com>
2020-03-22 17:28:09 -0300

    Fixing produtos duplicados no carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -6,12 +6,20 @@ import {
 } from "react-icons/md";
 
 import {
+  useDispatch,
+  useSelector
+} from "react-redux";
+
+import {
   Container,
   ProductTable,
   Total
 } from './styles';
 
 export default function Card() {
+
+  const cart = useSelector(state => state.cart);
+
   return (
     <Container>
       <ProductTable>
@@ -25,34 +33,36 @@ export default function Card() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <img src="https://images-na.ssl-images-amazon.com/images/I/41h9X8HpvEL._SX425_.jpg" alt="produto"/>
-            </td>
-            <td>
-              <strong>Xioami Redmi 8</strong>
-              <span>R$1099,99</span>
-            </td>
-            <td>
-              <div>
+          { cart.map(product => (
+            <tr>
+              <td>
+                <img src={product.image} alt={product.title}/>
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormatted}</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  </button>
+                  <input type="number" readOnly value={product.amount}/>
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="7159c1" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>R$2198,98</strong>
+              </td>
+              <td>
                 <button type="button">
-                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  <MdDelete size={20} color="#7159c1" />
                 </button>
-                <input type="number" readOnly value={2}/>
-                <button type="button">
-                  <MdAddCircleOutline size={20} color="7159c1" />
-                </button>
-              </div>
-            </td>
-            <td>
-              <strong>R$2198,98</strong>
-            </td>
-            <td>
-              <button type="button">
-                <MdDelete size={20} color="#7159c1" />
-              </button>
-            </td>
-          </tr>
+              </td>
+            </tr>
+          )) }
         </tbody>
       </ProductTable>
 
--- src/store/mudulos/card/reducer.js
+++ src/store/mudulos/card/reducer.js
@@ -2,7 +2,19 @@ export default function cart(state = [], action) {
   console.log(state);
   switch (action.type) {
     case 'ADD_TO_CART':
-      return[...state, action.product];
+      let productIndex = state.findIndex(p => p.id === action.product.id);
+      if(productIndex >= 0) {
+        let products = state;
+        products[productIndex].amount += 1;
+        return [...products];
+      } else {
+        return [...state, {
+          ...action.product,
+          amount: 1,
+        }];
+      }
+
+
 
     default:
       return state;
